55c2a6974c5e7cd99ace96d68d038ed4991627f4
Kugan <kugapriyanj97@gmail.com>
2024-12-04 01:37:47 +0530

    fixed addCart issue

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 import { ShoppingCart } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { IProductCardProps } from '../types/product';
+import { useAuth } from '../contexts/AuthContext';
 
 
 const ProductCard = ({ product, onAddToCart }:IProductCardProps) => {
-
+  const { isAuthenticated } = useAuth();
   
     const navigate = useNavigate();
     return (
@@ -24,7 +25,7 @@ const ProductCard = ({ product, onAddToCart }:IProductCardProps) => {
            
           </div>
           <button 
-              onClick={() => onAddToCart({...product, quantity: 1, size: product.size[0] || null, color: product.colors[0] || null})}
+              onClick={() => isAuthenticated? onAddToCart({...product, quantity: 1, size: product?.sizes[0] || null, color: product?.colors[0].name || null}):navigate('/login')}
               className="bg-blue-500 w-full mt-2 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors flex items-center"
             >
               <ShoppingCart className="mr-2" size={18} />
